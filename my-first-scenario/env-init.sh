d39daf1cdf14587b4060b0b08220cd36269bf9a8
root <root@163-172-48-162.online.net>
2018-07-30 21:24:56 +0200

    test

--- my-first-scenario/env-init.sh
+++ my-first-scenario/env-init.sh
@@ -1,3 +1,4 @@
 echo "L'environnement est en cours d'initialisation"
+mkdir /home/scrapbook/.ssh && mv /home/scrapbook/id_rsa /home/scrapbook/.ssh
 docker run -d jehage/centos7
 echo "L'environement est prêt!"
